fe097cb6eb2ded1930402bf9aed53152ad0ef7ce
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-10 17:37:23 -0400

    Implementación Prueba-Email 5 - test correción4? - alteernativa - biometria cygnus

--- data/empresas.py
+++ data/empresas.py
@@ -14,5 +14,13 @@ EMPRESAS = [
         "filtro_cargo":False,
         "Cargo": None,
 	"file_name": "BIOMETRIA"
+    },
+    {
+        "rut": "77128770-0",
+        "nombre": "CYGNUS (Alvarez Recursos Humanos Limitada)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":True,
+        "Cargo": "Mercaderista Planta",
+	"file_name": "CYGNUS"
     }
 ]
