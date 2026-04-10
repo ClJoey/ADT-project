4120e567472230b411e709aea98b93e5d32769a5
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-10 17:35:34 -0400

    Implementación Prueba-Email 5 - test correción4? - alteernativa - biometria

--- data/empresas.py
+++ data/empresas.py
@@ -1,5 +1,12 @@
 EMPRESAS = [
-
+    {
+        "rut": "79777010-8",
+        "nombre": "ALTERNATTIVA (MARKETING Y PROMOCIONES S.A)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":True,
+        "Cargo": "Mercaderista Planta",
+	"file_name": "ALTERNATTIVA"
+    },
     {
         "rut": "76257834-4",
         "nombre": "Biometria Aplicada Spa",
@@ -7,13 +14,5 @@ EMPRESAS = [
         "filtro_cargo":False,
         "Cargo": None,
 	"file_name": "BIOMETRIA"
-    },
-    {
-        "rut": "77091268-7",
-        "nombre": "NUEVA BIOMETRIA SPA",
-        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
-        "filtro_cargo":False,
-        "Cargo": None,
-	"file_name": "NUEVA_BIOMETRIA"
     }
 ]
