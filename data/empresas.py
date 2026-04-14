50d8cc61c23c03fcffa0310aeb529bf466cbe2b0
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-14 11:56:05 -0400

    Implementación Prueba-Email 8 nuevo prueba - Alternativa - prueba correciones cuando no hay datos 2-1

--- data/empresas.py
+++ data/empresas.py
@@ -2,7 +2,7 @@ EMPRESAS = [
     {
         "rut": "76788120-7",
         "nombre": "SK CAPACITACION (SK CAPACITACION S.A.)",
-        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "reportes": ["diario", "incidentes"],
         "filtro_cargo":False,
         "Cargo": None,
 	"file_name": "SK_CAPACITACION",
