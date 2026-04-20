b173eea982350b827d1b7ba599cd6da55244866a
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-20 14:49:42 -0400

    Prueba 1

--- data/empresas.py
+++ data/empresas.py
@@ -1,11 +1,38 @@
 EMPRESAS = [
     {
-        "rut": "79777010-8",
-        "nombre": "ALTERNATTIVA (MARKETING Y PROMOCIONES S.A)",
+        "rut": "92604000-6",
+        "nombre": "Empresa Nacional del Petroleo",
         "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
-        "filtro_cargo":True,
-        "Cargo": "Mercaderista Planta",
-	"file_name": "ALTERNATTIVA",
-	"url_menu": "https://alternattivadt.baplicada.cl/Default.aspx"
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "EMPRESA_PETROLEO",
+	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "87756500-9",
+        "nombre": "ENAP REFINERIAS S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_REFINERIAS",
+	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "96579730-0",
+        "nombre": "Enap Sipetrol S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_SIPETROL",
+	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
+    },
+    {
+        "rut": "77091268-7",
+        "nombre": "NUEVA BIOMETRIA SPA",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "NUEVA_BIOMETRIA",
+	"url_menu": "https://badt.baplicada.cl/Default.aspx"
     }
 ]
