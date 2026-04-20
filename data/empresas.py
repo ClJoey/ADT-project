06b07144069a12fa1fb4ec3ad43eaa32baadcda4
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-20 15:18:59 -0400

    Prueba 1-2 Aplicando Error ConnectionString - 1

--- data/empresas.py
+++ data/empresas.py
@@ -9,21 +9,21 @@ EMPRESAS = [
 	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
     },
     {
+        "rut": "77091268-7",
+        "nombre": "NUEVA BIOMETRIA SPA",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "NUEVA_BIOMETRIA",
+	"url_menu": "https://badt.baplicada.cl/Default.aspx"
+    },
+    {
         "rut": "87756500-9",
         "nombre": "ENAP REFINERIAS S.A",
         "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
         "filtro_cargo":False,
         "Cargo": None,
 	"file_name": "ENAP_REFINERIAS",
-	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
-    },
-    {
-        "rut": "96579730-0",
-        "nombre": "Enap Sipetrol S.A",
-        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
-        "filtro_cargo":False,
-        "Cargo": None,
-	"file_name": "ENAP_SIPETROL",
 	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
     },
     {
@@ -34,5 +34,15 @@ EMPRESAS = [
         "Cargo": None,
 	"file_name": "NUEVA_BIOMETRIA",
 	"url_menu": "https://badt.baplicada.cl/Default.aspx"
-    }
+    },
+    {
+        "rut": "96579730-0",
+        "nombre": "Enap Sipetrol S.A",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "ENAP_SIPETROL",
+	"url_menu": "https://enapdt.baplicada.cl/Default.aspx"
+    },
+    
 ]
