cf2e342a33ef1ea25e98e9de35e94e2bc743405b
Joseph <joseph.cervantes@inacapmail.cl>
2026-04-23 13:56:41 -0400

    data: restaurar todas las empresas para test completo

--- data/empresas.py
+++ data/empresas.py
@@ -1,5 +1,32 @@
 EMPRESAS = [
     {
+        "rut": "79777010-8",
+        "nombre": "ALTERNATTIVA (MARKETING Y PROMOCIONES S.A)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":True,
+        "Cargo": "Mercaderista Planta",
+	"file_name": "ALTERNATTIVA",
+	"nom_informe": "MARKETING Y PROMOCIONES S.A."
+    },
+    {
+        "rut": "76257834-4",
+        "nombre": "Biometria Aplicada Spa",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "BIOMETRIA",
+	"nom_informe": "Biometria Aplicada Spa"
+    },
+    {
+        "rut": "77128770-0",
+        "nombre": "CYGNUS (Alvarez Recursos Humanos Limitada)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":True,
+        "Cargo": "Mercaderista Planta",
+	"file_name": "CYGNUS",
+	"nom_informe": "Cygnus PDV"
+    },
+    {
         "rut": "92604000-6",
         "nombre": "Empresa Nacional del Petroleo",
         "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
@@ -52,6 +79,15 @@ EMPRESAS = [
         "Cargo": None,
 	"file_name": "SIGDO_KOOPERS_SA",
 	"nom_informe": "SOCIEDADES DE INVERSION Y RENTISTAS DE CAPITALES MOBILIARIOS EN GENERAL"
+    },
+    {
+        "rut": "78167971-2",
+        "nombre": "SIO RRHH (Outsourcing Integrales de Servicios S.A.)",
+        "reportes": ["asistencia", "jor_diaria", "domingos", "modificaciones", "diario", "incidentes"],
+        "filtro_cargo":False,
+        "Cargo": None,
+	"file_name": "SIO_RRHH",
+	"nom_informe": "Outsourcing Integrales de Servicios S.A."
     },
     {
         "rut": "76788120-7",
